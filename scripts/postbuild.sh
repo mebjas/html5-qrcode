2dc14b3cb74aad770976e0801ebace6d9294794c
Joost <Joost@tmmrman.nl>
2021-04-19 15:12:10 +0200

    Changes new lines to multiple echo statements

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,15 +1,19 @@
 ## Post build script
 echo 'Initiating post-build sequence'
 
-cp third_party/zxing-js.umd.min.js minified/html5-qrcode.min.js
+echo '/** ZXing **/' > minified/html5-qrcode.min.js
+cat third_party/zxing-js.umd.min.js >> minified/html5-qrcode.min.js
 echo 'Copied third_party/zxing-js.umd.min.js to minified/html5-qrcode.min.js'
 
 ## Copy all other temp files to final minified script
+
+echo  >> minified/html5-qrcode.min.js
+echo '/** Html5Qrcode **/' >> minified/html5-qrcode.min.js
 cat minified/html5-qrcode.tmp.js >> minified/html5-qrcode.min.js
 echo 'Copied minified/html5-qrcode.tmp.js to minified/html5-qrcode.min.js'
 
-echo '\n\n/** Html5QrcodeScanner **/' >> minified/html5-qrcode.min.js
-
+echo  >> minified/html5-qrcode.min.js
+echo '/** Html5QrcodeScanner **/' >> minified/html5-qrcode.min.js
 cat minified/html5-qrcode-scanner.tmp.js >> minified/html5-qrcode.min.js
 echo 'Copied minified/html5-qrcode-scanner.tmp.js to minified/html5-qrcode.min.js'
 
