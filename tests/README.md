2d720dc16ea87650a106dc65c3d3205a2c4590f7
minhaz <minhazav@gmail.com>
2021-12-12 00:22:04 +0800

    Setup basic unit tests with Mocha/Chai (#364)
    * Setup basic unit tests with Mocha/Chai
    
     - Setup basic test setup (unit tests)
     - Remove older testing framework based on Mocha and phantomjs
     - Remove Travic CI config as its no longer working seemlessly
    
    * More tests and some codacy fixes
    
    * Add tests for experimental-features
    
    * Better documentation

---
+++ tests/README.md
@@ -0,0 +1,22 @@
+# Html5-Qrcode testing
+
+Testing is very limited at the moment. As we moved from javascript based code to Typescript all the tests were cleaned up. Expecting to write high coverage unit tests for all the source code over time.
+
+If you are interested in contrbuting to the project, adding unit tests would be a great way to get comfortable with the codebase and would be highly impactful for the reliability and stability of the project.
+
+## Testing goals
+-   [ ] `P1` Unit tests
+    -   [ ] High coverage
+        -   [ ] `P1` Coverage > 70%
+        -   [ ] `P2` Coverage > 80%
+        -   [ ] `P3` Coverage > 90%
+
+    -   [ ] Add integration for CI (Travis no longer working).
+
+    -   [ ] Add integration for code coverage
+
+    -   [ ] Integrate code coverage with Codacy
+
+-   [ ] `P2` Browser based e2e testing
+
+-   [ ] `P2` Testing with BrowserStack
---
+++ tests/core.test.ts
@@ -0,0 +1,92 @@
+import { expect } from "chai";
+
+import {
+    Html5QrcodeSupportedFormats,
+    isValidHtml5QrcodeSupportedFormats,
+    QrcodeResultFormat,
+    Html5QrcodeResultFactory,
+    QrcodeResult,
+    DecodedTextType,
+    Html5QrcodeErrorFactory,
+    Html5QrcodeErrorTypes,
+    isNullOrUndefined
+} from "../src/core";
+
+describe("isValidHtml5QrcodeSupportedFormats function", () => {
+    it("should return true for supported formats", () => {
+        expect(isValidHtml5QrcodeSupportedFormats("AZTEC")).to.be.true;
+        expect(isValidHtml5QrcodeSupportedFormats("QR_CODE")).to.be.true;
+        expect(isValidHtml5QrcodeSupportedFormats("UPC_A")).to.be.true;
+        expect(isValidHtml5QrcodeSupportedFormats("EAN_8")).to.be.true;
+    });
+
+    it("should return false for unsupported formats", () => {
+        expect(isValidHtml5QrcodeSupportedFormats("random")).to.be.false;
+        expect(isValidHtml5QrcodeSupportedFormats("")).to.be.false;
+
+    });
+});
+
+describe("QrcodeResultFormat class", () => {
+    it("toString() should return correct string value", () => {
+        let qrcodeResultFormat = QrcodeResultFormat.create(
+            Html5QrcodeSupportedFormats.AZTEC);
+    
+        expect(qrcodeResultFormat.toString()).to.equal("AZTEC");
+    
+        qrcodeResultFormat = QrcodeResultFormat.create(
+            Html5QrcodeSupportedFormats.DATA_MATRIX);
+        expect(qrcodeResultFormat.toString()).to.equal("DATA_MATRIX");
+    });
+});
+
+describe("Html5QrcodeResultFactory class", () => {
+    it("createFromText has result and decodedText", () => {
+        let decodedText = "hello";
+        let qrcodeResult = Html5QrcodeResultFactory.createFromText(decodedText);
+        expect(qrcodeResult.decodedText).to.equal(decodedText);
+        expect(qrcodeResult.result.text).to.equal(decodedText);
+    });
+
+    it("createFromQrcodeResult has result and decodedText", () => {
+        let decodedText = "https://scanapp.org";
+        let inputQrcodeResult: QrcodeResult = {
+            text: decodedText,
+            format: QrcodeResultFormat.create(Html5QrcodeSupportedFormats.DATA_MATRIX),
+            decodedTextType: DecodedTextType.URL
+        }
+        let qrcodeResult = Html5QrcodeResultFactory.createFromQrcodeResult(inputQrcodeResult);
+        expect(qrcodeResult.decodedText).to.equal(decodedText);
+        expect(qrcodeResult.result).to.equal(inputQrcodeResult);
+    });
+});
+
+describe("Html5QrcodeErrorFactory class", () => {
+    it("createFrom() creates corresponding error object", () => {
+        let error = "test error message";
+        let qrcodeError = Html5QrcodeErrorFactory.createFrom(error);
+
+        expect(qrcodeError.errorMessage).to.equal(error);
+        expect(qrcodeError.type).to.equal(Html5QrcodeErrorTypes.UNKWOWN_ERROR);
+    });
+});
+
+// TODO(mebjas): Add support for proxy/spy for testing BaseLogger class.
+
+describe("isNullOrUndefined function", () => {
+    it("obj is null, undefined, returns true", () => {
+        expect(isNullOrUndefined(null)).to.be.true;
+        let obj;
+        expect(isNullOrUndefined(obj)).to.be.true;
+        obj = undefined;
+        expect(isNullOrUndefined(obj)).to.be.true;
+    });
+
+    it("obj is defined, returns true", () => {
+        let obj = "something";
+        expect(isNullOrUndefined(obj)).to.be.false;
+        expect(isNullOrUndefined(0)).to.be.false;
+        expect(isNullOrUndefined(-1)).to.be.false;
+        expect(isNullOrUndefined("undefined")).to.be.false;
+    });
+});
