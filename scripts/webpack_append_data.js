d3cc1afc85f202763c13842eb89f0b8177ec31eb
minhaz <minhazav@gmail.com>
2022-04-02 14:51:44 +0800

    Add version 2.2.1 with some refactors to code
    Change log.
    
    ### Version 2.2.1
     - Added support for `supportedScanType` in `Html5QrcodeScanner`. This feature
        was implemented by our latest contributor - [mohsinaav@](https://github.com/mohsinaav)
    
       Now users can decide to only use camera based scan or file based scan or use
       them in different order. How to use:
    
       ```js
        function onScanSuccess(decodedText, decodedResult) {
            // handle the scanned code as you like, for example:
            console.log(`Code matched = ${decodedText}`, decodedResult);
        }
    
        let config = {
            fps: 10,
            qrbox: {width: 100, height: 100},
            rememberLastUsedCamera: true,
            // Only support camera scan type.
            supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
        };
    
        let html5QrcodeScanner = new Html5QrcodeScanner(
            "reader", config, /* verbose= */ false);
        html5QrcodeScanner.render(onScanSuccess);
       ```
    
       For file based scan only choose:
       ```js
       supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_FILE]
       ```
    
       For supporting both as it is today, you can ignore this field or set as:
       ```js
       supportedScanTypes: [
           Html5QrcodeScanType.SCAN_TYPE_CAMERA,
            Html5QrcodeScanType.SCAN_TYPE_FILE]
       ```
    
       To set the file based scan as defult change the order:
       ```js
          supportedScanTypes: [
            Html5QrcodeScanType.SCAN_TYPE_FILE,
            Html5QrcodeScanType.SCAN_TYPE_CAMERA]
       ```

--- scripts/webpack_append_data.js
+++ scripts/webpack_append_data.js
@@ -1,8 +1,18 @@
 /** Append the libary components to globals for backwards compatibility. */
-if (window && !Html5QrcodeScanner) {
-    var Html5QrcodeScanner = window.__Html5QrcodeLibrary__.Html5QrcodeScanner;
-    var Html5Qrcode = window.__Html5QrcodeLibrary__.Html5Qrcode;
-    var Html5QrcodeSupportedFormats = window.__Html5QrcodeLibrary__.Html5QrcodeSupportedFormats
-    var Html5QrcodeScannerState = window.__Html5QrcodeLibrary__.Html5QrcodeScannerState;
-    var Html5QrcodeScanType = window.__Html5QrcodeLibrary__.Html5QrcodeScanType;
+if (window) {
+    if (!Html5QrcodeScanner) {
+        var Html5QrcodeScanner = window.__Html5QrcodeLibrary__.Html5QrcodeScanner;
+    }
+    if (!Html5Qrcode) {
+        var Html5Qrcode = window.__Html5QrcodeLibrary__.Html5Qrcode;
+    }
+    if (!Html5QrcodeSupportedFormats) {
+        var Html5QrcodeSupportedFormats = window.__Html5QrcodeLibrary__.Html5QrcodeSupportedFormats
+    }
+    if (!Html5QrcodeScannerState) {
+        var Html5QrcodeScannerState = window.__Html5QrcodeLibrary__.Html5QrcodeScannerState;
+    }
+    if (!Html5QrcodeScanType) {
+        var Html5QrcodeScanType = window.__Html5QrcodeLibrary__.Html5QrcodeScanType;
+    }
 }
