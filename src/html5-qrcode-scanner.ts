b7856276f8cc1103b3ce6c1e312f1f046a95af08
Themistoklis Chatziemmanouil <themis.chatzie@gmail.com>
2023-01-30 17:59:01 +0200

    fix: race condition
    Fix race condition when render/clear is called in rapid succession. This issue was first spotted when trying to use this library in react:
    Relevant issue : https://github.com/briosheje/react-html5-qrcode-reader/issues/2

--- src/html5-qrcode-scanner.ts
+++ src/html5-qrcode-scanner.ts
@@ -178,6 +178,7 @@ export class Html5QrcodeScanner {
     private persistedDataManager: PersistedDataManager;
     private scanTypeSelector: ScanTypeSelector;
     private logger: Logger;
+    private isCleared = false;
 
     // Initally null fields.
     private html5Qrcode: Html5Qrcode | undefined;
@@ -232,6 +233,7 @@ export class Html5QrcodeScanner {
     public render(
         qrCodeSuccessCallback: QrcodeSuccessCallback,
         qrCodeErrorCallback: QrcodeErrorCallback | undefined) {
+        this.isCleared = false;
         this.lastMatchFound = null;
 
         // Add wrapper to success callback.
@@ -324,6 +326,8 @@ export class Html5QrcodeScanner {
      *  fails otherwise.
      */
     public clear(): Promise<void> {
+        this.isCleared = true;
+        
         const emptyHtmlContainer = () => {
             const mainContainer = document.getElementById(this.elementId);
             if (mainContainer) {
@@ -546,6 +550,9 @@ export class Html5QrcodeScanner {
         }
 
         Html5Qrcode.getCameras().then((cameras) => {
+            if ($this.isCleared) {
+                return;
+            }
             // By this point the user has granted camera permissions.
             $this.persistedDataManager.setHasPermission(
                 /* hasPermission */ true);
@@ -612,6 +619,9 @@ export class Html5QrcodeScanner {
             && this.persistedDataManager.hasCameraPermissions()) {
             CameraPermissions.hasPermissions().then(
                 (hasPermissions: boolean) => {
+                if ($this.isCleared) {
+                    return;
+                }
                 if (hasPermissions) {
                     $this.createCameraListUi(
                         scpCameraScanRegion, requestPermissionContainer);
@@ -964,6 +974,9 @@ export class Html5QrcodeScanner {
             CameraPermissions.hasPermissions().then(
                 (hasPermissions: boolean) => {
                 if (hasPermissions) {
+                    if ($this.isCleared) {
+                        return;
+                    }
                     // Start feed.
                     // Assuming at this point the permission button exists.
                     let permissionButton = document.getElementById(
