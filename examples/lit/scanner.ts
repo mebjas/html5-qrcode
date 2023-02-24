08809852c63352b73a9e45dc44bb0e5aaaf632b9
Markus Fürer <m.fuerer@vtxmail.ch>
2023-02-24 10:15:21 +0100

    chore: :memo: add lit element example

---
+++ examples/lit/scanner.ts
@@ -0,0 +1,58 @@
+import { css, html, LitElement, TemplateResult } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+import { query } from 'lit-element';
+import { Html5QrcodeError, Html5QrcodeResult } from 'html5-qrcode/esm/core';
+import { Html5QrcodeScanner } from 'html5-qrcode';
+
+@customElement('scanner-element')
+export class ScannerElement extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+    `,
+  ];
+
+  @query('#reader')
+  reader: HTMLElement;
+
+  protected render(): TemplateResult {
+    return html` <div id="reader"></div> `;
+  }
+
+  protected firstUpdated(): void {
+    const onScanSuccess = (
+      decodedText: string,
+      decodedResult: Html5QrcodeResult
+    ) => {
+      // handle the scanned code as you like
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+    };
+
+    const onScanFailure = (errorMessage: string, error: Html5QrcodeError) => {
+      // handle scan failure, usually better to ignore and keep scanning
+      console.warn(`Code scan error = ${errorMessage}`, error);
+    };
+
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+      this.reader,
+      {
+        fps: 10,
+        qrbox: {
+          width: 350,
+          height: 250,
+        },
+      },
+      false
+    );
+
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'scanner-element': ScannerElement;
+  }
+}
