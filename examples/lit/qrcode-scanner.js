15129b7f0cdb18a91df5119294083a161e2ef955
Markus Fürer <m.fuerer@vtxmail.ch>
2023-02-24 15:02:48 +0100

    fix: :rotating_light: fix codacy warnings

--- examples/lit/qrcode-scanner.js
+++ examples/lit/qrcode-scanner.js
@@ -1,5 +1,5 @@
-import { html, css, LitElement } from 'https://unpkg.com/lit?module';
-import { Html5QrcodeScanner } from 'https://unpkg.com/html5-qrcode?module';
+import { html, css, LitElement } from "https://unpkg.com/lit?module";
+import { Html5QrcodeScanner } from "https://unpkg.com/html5-qrcode?module";
 
 export class QRCodeScanner extends LitElement {
   static styles = css`
@@ -9,18 +9,22 @@ export class QRCodeScanner extends LitElement {
   `;
 
   render() {
-    return html`<div id="reader"></div>`;
+    return html`
+      <div id="reader"></div>
+      <div>${this.decodedText}</div>
+      <div>${this.errorMessage}</div>
+    `;
   }
 
   firstUpdated() {
     const onScanSuccess = (decodedText, decodedResult) => {
       // handle the scanned code as you like
-      console.log(`Code matched = ${decodedText}`, decodedResult);
+      this.decodedText = decodedText;
     };
 
     const onScanFailure = (errorMessage, error) => {
       // handle scan failure, usually better to ignore and keep scanning
-      console.warn(`Code scan error = ${errorMessage}`, error);
+      this.errorMessage = errorMessage;
     };
 
     const config = {
@@ -31,11 +35,11 @@ export class QRCodeScanner extends LitElement {
       },
     };
 
-    const reader = this.shadowRoot.querySelector('#reader');
+    const reader = this.shadowRoot.querySelector("#reader");
     const html5QrcodeScanner = new Html5QrcodeScanner(reader, config);
 
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
   }
 }
 
-customElements.define('qrcode-scanner', QRCodeScanner);
+customElements.define("qrcode-scanner", QRCodeScanner);
